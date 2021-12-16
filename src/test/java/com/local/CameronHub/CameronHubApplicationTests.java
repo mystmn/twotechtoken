45e6617814ed4778b16d6eb282d644c8b6b5e37b
Paul Cameron <paul.cameron@accenture.com>
2021-12-15 23:24:53 -0500

    setup complete

---
+++ src/test/java/com/local/CameronHub/CameronHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.CameronHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CameronHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
